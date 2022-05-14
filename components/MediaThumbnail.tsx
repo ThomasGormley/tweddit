42f53435bc72f6009fca30046a0b0825c7014733
Thomas Gormley <thomasgormley415@gmail.com>
2022-05-14 22:00:25 +0100

    add SearchBar component

--- components/MediaThumbnail.tsx
+++ components/MediaThumbnail.tsx
@@ -1,18 +1,20 @@
 import React, { Fragment } from "react";
+import { Preview } from "../types/ThreadsResult";
 
 type MediaThumbnailProps = {
-    preview: any;
+    preview: Preview;
 };
 
 export default function MediaThumbnail({ preview }: MediaThumbnailProps) {
     return (
         <Fragment>
-            {preview.images.map((img: unknown, i: number) => {
+            {preview.images.map((img) => {
                 return (
+                    // eslint-disable-next-line @next/next/no-img-element
                     <img
-                        key={i}
+                        key={img.resolutions[2]?.url}
                         src={img.resolutions[2]?.url.replaceAll("&amp;", "&")}
-                        className="mt-2 rounded-xl border border-dim-border"
+                        className="mt-2 rounded-xl w-full border border-dim-border"
                     />
                 );
             })}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Banner from "../components/Banner";
 import { Feed } from "../components/Feed";
+import SearchBar from "../components/SearchBar";
 import BasePage, {
     getServerSideProps as baseGetServerSideProps,
 } from "./base.page";
@@ -15,34 +16,8 @@ function Index() {
                     </div>
 
                     {/* Search column */}
-                    <div className="mr-[10px] hidden md:flex md:w-[290px] pt-[6px] lg:w-[350px]">
-                        <div className="fixed flex w-[inherit] flex-col">
-                            <div className="">
-                                <form
-                                    aria-label="Search Tweddit"
-                                    role="search"
-                                    className="w-full rounded-full  bg-[#273340] text-off-white flex justify-center items-center"
-                                >
-                                    <div className="ml-[8px]">
-                                        <svg
-                                            viewBox="0 0 24 24"
-                                            aria-hidden="true"
-                                            fill="currentColor"
-                                            className="w-6 h-6 min-w-[32px] text-[#8b98a5] pl-[12px]"
-                                        >
-                                            <g>
-                                                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                                            </g>
-                                        </svg>
-                                    </div>
-                                    <input
-                                        type="search"
-                                        placeholder="Search Tweddit"
-                                        className="w-full bg-transparent p-[12px] ml-[2px] text-15px "
-                                    ></input>
-                                </form>
-                            </div>
-                        </div>
+                    <div className="mr-[10px] hidden pt-[6px] md:flex md:w-[290px] lg:w-[350px]">
+                        <SearchBar />
                     </div>
                 </div>
             </main>

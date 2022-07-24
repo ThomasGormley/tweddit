9ecb619935b8705dc6b13997f790aa72606d3a49
Thomas Gormley <thomasgormley415@gmail.com>
2022-07-24 19:54:14 +0100

    change image preview to use source

--- components/MediaThumbnail.tsx
+++ components/MediaThumbnail.tsx
@@ -9,11 +9,12 @@ export default function MediaThumbnail({ preview }: MediaThumbnailProps) {
     return (
         <Fragment>
             {preview.images.map((img) => {
+
                 return (
                     // eslint-disable-next-line @next/next/no-img-element
                     <img
-                        key={img.resolutions[2]?.url}
-                        src={img.resolutions[2]?.url.replaceAll("&amp;", "&")}
+                        key={img.source?.url}
+                        src={img.source?.url.replaceAll("&amp;", "&")}
                         className="mt-2 rounded-xl w-full border border-dim-border"
                     />
                 );

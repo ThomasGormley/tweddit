f83fb3ff8a2c8d900a3bfd2e661cd0a3f7484fbc
Thomas Gormley <thomas.gormley@firstup.io>
2022-11-06 21:02:43 +0000

    move subreddit switcher to new file

--- src/components/current-subreddit-banner.tsx
+++ src/components/current-subreddit-banner.tsx
@@ -1,16 +1,6 @@
-import useRedditQuery from "@/hooks/use-reddit-query";
-import useSubredditsSearch from "@/hooks/use-subreddits-search";
-import { Combobox, Transition } from "@headlessui/react";
-import {
-    CheckIcon,
-    ChevronDownIcon,
-    ChevronUpDownIcon,
-} from "@heroicons/react/20/solid";
-import clsx from "clsx";
 import { useRouter } from "next/router";
-import { Fragment, useState } from "react";
-import { useDebounce } from "usehooks-ts";
 import Banner from "./banner";
+import { SubredditSwitcher } from "./subreddit-switcher";
 import UserAvatar from "./user-avatar";
 
 export function CurrentSubredditBanner({}) {
@@ -42,97 +32,6 @@ export function CurrentSubredditBanner({}) {
                 </div>
             </div>
         </Banner>
-    );
-}
-
-// const people = [
-//     { id: 1, name: "Durward Reynolds" },
-//     { id: 2, name: "Kenton Towne" },
-//     { id: 3, name: "Therese Wunsch" },
-//     { id: 4, name: "Benedict Kessler" },
-//     { id: 5, name: "Katelyn Rohan" },
-// ];
-
-function SubredditSwitcher({ current }: { current: string }) {
-    const [selected, setSelected] = useState(current);
-    const [query, setQuery] = useState("");
-    const searchText = useDebounce(query, 250);
-    const { data } = useSubredditsSearch(searchText);
-    console.log({ data });
-
-    const subredditNames =
-        data?.data.children.map(
-            (subreddit) => subreddit.data.display_name_prefixed,
-        ) ?? [];
-    // const filteredPeople =
-    //     query === ""
-    //         ? people
-    //         : people.filter((person) =>
-    //               person.name
-    //                   .toLowerCase()
-    //                   .replace(/\s+/g, "")
-    //                   .includes(query.toLowerCase().replace(/\s+/g, "")),
-    //           );
-
-    return (
-        <Combobox value={selected} onChange={setSelected}>
-            <div className="mt-1 sm:relative">
-                <div className="flex w-full cursor-default overflow-hidden font-bold focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-17px  sm:relative">
-                    <Combobox.Input
-                        className="w-full sm:w-auto border-none bg-transparent py-2 outline-none focus:ring-0 text-center sm:text-start"
-                        // displayValue={(person: typeof people[number]) =>
-                        //     person.name
-                        // }
-                        onFocus={() => setSelected("")}
-                        onBlur={() => setSelected(current)}
-                        onChange={(event) => setQuery(event.target.value)}
-                        placeholder='Subreddit...'
-                    />
-                    <Combobox.Button className="flex items-center">
-                        <ChevronDownIcon
-                            className="h-5 w-5 text-gray-400"
-                            aria-hidden="true"
-                        />
-                    </Combobox.Button>
-                </div>
-                <Transition
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    afterLeave={() => setQuery("")}
-                >
-                    <Combobox.Options className="glow absolute left-2 right-2 mt-1 max-h-60 overflow-auto rounded-md bg-dim text-base text-off-white shadow-glow shadow-off-white/30 ring-1 ring-off-white/10">
-                        {subredditNames.length === 0 ? (
-                            <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                                Nothing found.
-                            </div>
-                        ) : (
-                            subredditNames.map((name) => (
-                                <Combobox.Option
-                                    key={name}
-                                    className={({ active }) =>
-                                        clsx(
-                                            `relative cursor-default select-none rounded-md  py-2 pl-10 pr-4`,
-                                            active && "bg-[#1E2732]",
-                                        )
-                                    }
-                                    value={name}
-                                >
-                                    {({ selected, active }) => (
-                                        <>
-                                            <span className={`block truncate `}>
-                                                {name}
-                                            </span>
-                                        </>
-                                    )}
-                                </Combobox.Option>
-                            ))
-                        )}
-                    </Combobox.Options>
-                </Transition>
-            </div>
-        </Combobox>
     );
 }
 
